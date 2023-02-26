676309d35037efeee7b7ab666f6f0cb2158e8aea
Jean <jeanldc1705@gmail.com>
2023-02-26 18:20:42 -0500

    update to market making

--- market_making.py
+++ market_making.py
@@ -1,3 +1,4 @@
+#https://kollider.medium.com/build-a-crypto-market-making-bot-in-python-d71eeae2dcd7
 from typing import Dict, List
 from datamodel import OrderDepth, TradingState, Order
 
@@ -11,7 +12,7 @@ class Trader:
         """
         # Initialize the method output dict as an empty dict
         result = {}
-        spreadcutoff = 5
+        spreadcutoff = 4
         # Iterate over all the keys (the available products) contained in the order depths
         for product in state.order_depths.keys():
             orders: list[Order] = []
@@ -19,15 +20,15 @@ class Trader:
             # Retrieve the Order Depth containing all the market BUY and SELL orders for PEARLS
             order_depth: OrderDepth = state.order_depths[product]
 
-            if len(order_depth.sell_orders) > 0 and len(order_depth.buy_orders) > 0:
+            if len(order_depth.sell_orders) > 0 and len(order_depth.buy_orders) > 0 and product=="BANANAS":
                 spread = min(order_depth.sell_orders.keys()) - max(order_depth.buy_orders.keys())
                 if spread > spreadcutoff:
                     #send an order slightly above the best bid and slightly below the best ask
                     best_bid = max(order_depth.buy_orders.keys())
                     best_ask = min(order_depth.sell_orders.keys())
                     orders = []
-                    orders.append(Order(product, best_bid + spread/4, 2))
-                    orders.append(Order(product, best_ask - spread/4, -2))
+                    orders.append(Order(product, best_bid + spread/3, 2))
+                    orders.append(Order(product, best_ask - spread/3, -2))
 
                 # Add all the above orders to the result dict
                 result[product] = orders
