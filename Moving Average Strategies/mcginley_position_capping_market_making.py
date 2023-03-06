d69c050de408759d02013ea89b8016ee2aa3bc41
jlavigne <jeanldc1705@gmail.com>
2023-03-06 13:29:41 -0500

    combined

---
+++ Moving Average Strategies/mcginley_position_capping_market_making.py
@@ -0,0 +1,120 @@
+from typing import Dict, List
+from datamodel import OrderDepth, TradingState, Order
+
+# Mean Reversion with Adaptive Moving Average (AMA), n = 10 (lookback 10 timesteps to adapt)
+# https://help.cqg.com/cqgic/23/default.htm#!Documents/adaptivemovingaverageama.htm
+
+class Trader:
+
+    POSITION_LIMIT = {"BANANAS" : 10, "PEARLS" : 10}
+    prices = {
+        "asks" : {}, 
+        "bids" : {},
+        "avg_prices" : {},
+        "count" : {},
+        "acceptable_price" : {
+            "PEARLS" : 10000,
+            "BANANAS" : 4895
+        } 
+    }
+
+    RISK_ADJUSTMENT = {"BANANAS" : 0.12, "PEARLS" : 0.12}
+    ORDER_VOLUME = {"BANANAS" : 4, "PEARLS" : 4}
+    HALF_SPREAD_SIZE = {"BANANAS": 3, "PEARLS": 3}
+
+    def run(self, state: TradingState) -> Dict[str, List[Order]]:
+        """
+        Only method required. It takes all buy and sell orders for all symbols as an input,
+        and outputs a list of orders to be sent
+        """
+        # Initialize the method output dict as an empty dict
+        result = {}
+
+        # Iterate over all the keys (the available products) contained in the order depths
+        for product in state.order_depths.keys():
+            order_depth: OrderDepth = state.order_depths[product]
+            orders: list[Order] = []
+
+
+            mcginley_price = self.prices["acceptable_price"][product]
+
+            if len(order_depth.sell_orders) > 0 and len(order_depth.buy_orders) > 0: # if there are orders in the market, recompute acceptable price
+
+                best_ask = min(order_depth.sell_orders.keys())
+                best_ask_volume = order_depth.sell_orders[best_ask]
+                best_bid = max(order_depth.buy_orders.keys())
+                best_bid_volume = order_depth.buy_orders[best_bid]
+                value = (best_ask + best_bid) / 2
+
+                try:
+                    position = state.position[product]
+                except:
+                    position = 0
+                    
+                skew = -position * self.RISK_ADJUSTMENT[product]
+
+                # buy_quote = value + (skew - spread/2+0.01)
+                # sell_quote = value + (skew + spread/2-0.01)
+                buy_quote = value - self.HALF_SPREAD_SIZE[product] + skew
+                sell_quote = value + self.HALF_SPREAD_SIZE[product] + skew
+    
+                orders.append(Order(product, buy_quote, self.ORDER_VOLUME[product]))
+                orders.append(Order(product, sell_quote, -self.ORDER_VOLUME[product]))
+
+
+
+                if product not in self.prices["asks"]:
+                    self.prices["asks"][product] = []
+                if product not in self.prices["bids"]:
+                    self.prices["bids"][product] = []
+
+                self.prices["asks"][product].append(best_ask)
+                self.prices["bids"][product].append(best_bid)
+
+                n = 10
+                k = 0.6
+                get_avg_price = lambda x : (self.prices["asks"][product][x] + self.prices["bids"][product][x]) / 2 # gets average price at index
+                curr_price = (best_ask + best_bid) / 2 
+
+                # first iteration
+                if product not in self.prices["avg_prices"]:
+                    self.prices["avg_prices"][product] = [curr_price]
+                    self.prices["acceptable_price"][product] = curr_price
+
+                    # don't place orders in the first iteration
+                    result[product] = orders
+                    return result
+                
+                i = len(self.prices["avg_prices"][product]) - 1 # index of current price
+                n_lookback_index = max(i - n, 0) # make sure we don't accidentally lookback into the negative indices
+
+                mcginley_price = mcginley_price + (curr_price-mcginley_price)/(k * n * (curr_price/mcginley_price)**4)
+
+                self.prices["acceptable_price"][product] = mcginley_price
+                self.prices["avg_prices"][product].append(mcginley_price)
+
+
+                # set acceptable price
+                acceptable_price = self.prices["acceptable_price"][product]
+
+                # based on pricing, make orders
+                if len(order_depth.sell_orders) > 0:
+                    best_ask = min(order_depth.sell_orders.keys())
+                    best_ask_volume = order_depth.sell_orders[best_ask]
+
+                    if best_ask < acceptable_price:
+                        print("BUY", str(-best_ask_volume) + "x", best_ask)
+                        orders.append(Order(product, best_ask, max(0,min(-best_ask_volume, self.POSITION_LIMIT[product] - position))))
+
+                if len(order_depth.buy_orders) > 0:
+                    best_bid = max(order_depth.buy_orders.keys())
+                    best_bid_volume = order_depth.buy_orders[best_bid]
+                    if best_bid > acceptable_price:
+                        print("SELL", str(best_bid_volume) + "x", best_bid)
+                        orders.append(Order(product, best_bid, -max(0,min(best_bid_volume, self.POSITION_LIMIT[product] + position))))
+
+                print(f'{product}, {best_ask}, {best_ask_volume}, {best_bid}, {best_bid_volume}, {acceptable_price}')
+
+                result[product] = orders
+
+        return result
