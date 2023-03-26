f96f440700cf5c35f0ea1dfe868880b5de3c0610
adiv413 <adivas612@gmail.com>
2023-03-26 17:04:54 -0400

    visualizer

--- Data Analysis/visualizer.py
+++ Data Analysis/visualizer.py
@@ -1,8 +1,8 @@
 from matplotlib import pyplot as plt
-from scipy.stats import pearsonr
+from scipy.stats import pearsonr, spearmanr
 import statsmodels.tsa.stattools as ts
 
-lines = open("Data Analysis/logs/day2.log").readlines()
+lines = open("Round 4/data/data.log").readlines()
 lines = [i for i in lines if ";" in i][1:]
 print(lines[0], lines[-1])
 products = {}
@@ -11,13 +11,23 @@ for i in lines:
     x = [j.strip() for j in x if j != '']
     if x[2] not in products:
         products[x[2]] = []
-    products[x[2]].append(float(x[-1]))
+    products[x[2]].append(float(x[-2]))
 
-for i in products.keys():
-    plt.plot(products[i])
-    plt.title(i)
-    plt.show()
+# for i in products.keys():
+    # plt.plot(products[i])
+    # plt.title(i)
+    # plt.show()
 
-# print(pearsonr(products["PINA_COLADAS"], products["COCONUTS"]))
-# result = ts.coint(products["PINA_COLADAS"], products["COCONUTS"])
-# print(result[1])
+assert(len(products["BAGUETTE"]) == len(products["DIP"]) == len(products["UKULELE"]))
+productsum = [products["BAGUETTE"][i] * 2 + products["DIP"][i] * 4 + products["UKULELE"][i] for i in range(len(products["UKULELE"]))]
+productdiff = [products["PICNIC_BASKET"][i] - productsum[i] for i in range(len(products["PICNIC_BASKET"]))]
+plt.plot(productdiff, label="diff")
+# plt.plot(products["PICNIC_BASKET"], label="real basket value")
+plt.legend()
+plt.show()
+
+print("picnic basket + estimation sum pearson", pearsonr(productsum, products["PICNIC_BASKET"]))
+print("picnic basket + estimation sum spearman", spearmanr(productsum, products["PICNIC_BASKET"]))
+
+print("dip + baguette pearson", pearsonr(products["DIP"], products["BAGUETTE"]))
+print("dip + baguette spearman", spearmanr(products["DIP"], products["BAGUETTE"]))
