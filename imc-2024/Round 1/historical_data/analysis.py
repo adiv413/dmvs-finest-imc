46e4eee441d7a4a72a4558227a5f6a0a1958fb68
Aditya Vasantharao <av286@duke.edu>
2024-04-11 03:24:29 -0400

    add lr_percentage

--- imc-2024/Round 1/historical_data/analysis.py
+++ imc-2024/Round 1/historical_data/analysis.py
@@ -8,78 +8,94 @@ df2 = pd.read_csv("prices_round_1_day_0.csv", header = 0, sep=";")
 df1 = pd.read_csv("prices_round_1_day_-1.csv", header = 0, sep=";")
 df0 = pd.read_csv("prices_round_1_day_-2.csv", header = 0, sep=";")
 
+# df0 = pd.read_csv("lr.csv", header = 0, sep=";")
 
 
-for lookback in range(2, 35):
-    x_amethysts = []
-    bid_amethysts = []
-    x_starfruit = []
-    bid_starfruit = []
-    y_starfruit = []
-    pred_starfruit = []
-    prev_starfruit = deque()
-    prev_time = deque()
-    ask_amethysts = []
+x_amethysts = []
+bid_amethysts = []
+x_starfruit = []
+bid_starfruit = []
+y_starfruit = []
+pred_starfruit = []
+prev_starfruit = deque()
+prev_time = deque()
+ask_amethysts = []
 
-    ask_starfruit = []
-    corrs = []
-    errors = []
 
-    amethyst_spreads = []
-    starfruit_spreads = []
+ask_starfruit = []
+corrs = []
+errors = []
 
-    adj = 0
-    for d in [df0, df1, df2]:
-        for index, row in d.iterrows():
-            if(row['product'] == "AMETHYSTS"):
-                x_amethysts.append(row['timestamp'] + adj)
-                bid_amethysts.append(row['bid_price_1'])
-                ask_amethysts.append(row['ask_price_1'])
+amethyst_spreads = []
+starfruit_spreads = []
 
-                amethyst_spreads.append(row['ask_price_1'] - row['bid_price_1'])
+indicators = []
+lookback = 12
+
+adj = 0
+for d in [df0, df1, df2]:
+    for index, row in d.iterrows():
+        if(row['product'] == "AMETHYSTS"):
+            x_amethysts.append(row['timestamp'] + adj)
+            bid_amethysts.append(row['bid_price_1'])
+            ask_amethysts.append(row['ask_price_1'])
+
+            amethyst_spreads.append(row['ask_price_1'] - row['bid_price_1'])
+        else:
+            x_starfruit.append(row['timestamp'] + adj)
+            bid_starfruit.append(row['bid_price_1'])
+            ask_starfruit.append(row['ask_price_1'])
+            y_starfruit.append(row['mid_price'])
+            starfruit_spreads.append(row['ask_price_1'] - row['bid_price_1'])
+
+            if len(prev_starfruit) < lookback + 1:
+                prev_starfruit.append(row['mid_price'])
+                prev_time.append(row['timestamp'])
+                pred_starfruit.append(row['mid_price'])
+                indicators.append(0)
             else:
-                x_starfruit.append(row['timestamp'] + adj)
-                bid_starfruit.append(row['bid_price_1'])
-                ask_starfruit.append(row['ask_price_1'])
-                y_starfruit.append(row['mid_price'])
-                starfruit_spreads.append(row['ask_price_1'] - row['bid_price_1'])
+                prev_starfruit_percentages = [(prev_starfruit[i + 1] - prev_starfruit[i])/prev_starfruit[i] + 1 for i in range(len(prev_starfruit) - 1)]
+                # print(prev_starfruit_percentages)
+                m, b = linear_regression(list(prev_time)[1:], prev_starfruit_percentages)
+                try:
+                    corr = correlation(list(prev_time)[1:], prev_starfruit_percentages) ** 2
+                except:
+                    corr = 1
+                corrs.append(corr)
+                pred = (m * row['timestamp'] + b) * prev_starfruit[-1]
+                # print(m * row['timestamp'] + b, pred)
+                pred_starfruit.append(pred)
+                error = abs(pred - row['mid_price'])
+                errors.append(error)
 
-                if len(prev_starfruit) < lookback:
-                    prev_starfruit.append(row['mid_price'])
-                    prev_time.append(row['timestamp'])
-                    pred_starfruit.append(row['mid_price'])
+                if error == 0:
+                    indicators.append(row['mid_price'] + 30)
                 else:
-                    m, b = linear_regression(list(prev_time), list(prev_starfruit))
-                    try:
-                        corr = correlation(list(prev_time), list(prev_starfruit)) ** 2
-                    except:
-                        corr = 1
-                    corrs.append(corr)
-                    pred = m * row['timestamp'] + b
-                    pred_starfruit.append(pred)
-                    error = abs(pred - row['mid_price'])
-                    errors.append(error)
+                    indicators.append(0)
 
-                    # print(pred, row['mid_price'], error, corr)
 
-                    prev_starfruit.popleft()
-                    prev_time.popleft()
-                    prev_starfruit.append(row['mid_price'])
-                    prev_time.append(row['timestamp'])
 
-                    
+                # print(pred, row['mid_price'], error, corr)
+
+                prev_starfruit.popleft()
+                prev_time.popleft()
+                prev_starfruit.append(row['mid_price'])
+                prev_time.append(row['timestamp'])
+
                 
+            
 
-        adj += 1000000
+    adj += 1000000
 
 
 
-    print("lookback:", lookback)
-    print("avg corr:", str(sum(corrs)/len(corrs)))
-    print("avg error:", str(sum(errors)/len(errors)))
-    z = [i for i in errors if i < 0.3]
-    print("accuracy:", str(len(z)/len(errors)))
-    print()
+
+print("lookback:", lookback)
+print("avg corr:", str(sum(corrs)/len(corrs)))
+print("avg error:", str(sum(errors)/len(errors)))
+z = [i for i in errors if i < 0.3]
+print("accuracy:", str(len(z)/len(errors)))
+print()
 
 
 # try lookback 10
@@ -98,14 +114,14 @@ for lookback in range(2, 35):
 # plt.ylabel('price')
 # plt.legend()
 # plt.show()
-# plt.plot(x_starfruit, bid_starfruit, label='starfruit_bid')
-# plt.plot(x_starfruit, ask_starfruit, label='starfruit_ask')
+plt.scatter(x_starfruit, bid_starfruit, label='starfruit_bid')
+plt.scatter(x_starfruit, ask_starfruit, label='starfruit_ask')
 # plt.plot(x_starfruit, y_starfruit, label='starfruit_mid')
-# plt.plot(x_starfruit, pred_starfruit, label='starfruit_pred')
+plt.scatter(x_starfruit, pred_starfruit, label='starfruit_pred')
 
 
 
-# plt.xlabel('timestamp')
-# plt.ylabel('price')
-# plt.legend()
-# plt.show()
+plt.xlabel('timestamp')
+plt.ylabel('price')
+plt.legend()
+plt.show()
---
+++ imc-2024/Round 1/lr_percentage.py
@@ -0,0 +1,261 @@
+#https://kollider.medium.com/build-a-crypto-market-making-bot-in-python-d71eeae2dcd7
+#https://www.reddit.com/r/algotrading/comments/6q8dp6/market_making_theory_and_application_readings/
+from typing import Dict, List, Any
+from datamodel import OrderDepth, TradingState, Order
+from math import floor
+import jsonpickle
+import collections
+from statistics import linear_regression
+class Trader:
+
+    products = ["AMETHYSTS", "STARFRUIT"]
+    POSITION_LIMIT = {"AMETHYSTS": 20, "STARFRUIT": 20}
+
+    AMETHYST_PRICE = 10000
+    STARFRUIT_PRICE = -1
+    
+    def market_make_amethyst(self, product, order_depth, position):
+        orders: list[Order] = []
+
+        osell = collections.OrderedDict(sorted(order_depth.sell_orders.items()))
+        obuy = collections.OrderedDict(sorted(order_depth.buy_orders.items(), reverse=True))
+        buyReserve = min(20, 20-position)
+        sellReserve = max(-20, -20-position)
+        ## Market Taking
+        #BUYING
+        print(f'Position: {position}')
+        for sellOrder in osell:
+            if sellOrder < self.AMETHYST_PRICE:
+                orders.append(Order(product, sellOrder, min(buyReserve,-osell[sellOrder])))
+                buyReserve -= min(buyReserve,-osell[sellOrder])
+                print(f'Market Taking Buy Order at {sellOrder} with volume {min(buyReserve,-osell[sellOrder])}')
+            elif sellOrder == self.AMETHYST_PRICE and position < 0:
+                orders.append(Order(product, sellOrder, min(buyReserve,-osell[sellOrder])))
+                buyReserve -= min(buyReserve,-osell[sellOrder])
+                print(f'Market Taking Buy Order at {sellOrder} with volume {min(buyReserve,-osell[sellOrder])}')
+               
+        #BUYING
+
+        #SELLING    
+        for buyOrder in obuy:
+            if buyOrder > self.AMETHYST_PRICE:
+                orders.append(Order(product, buyOrder, max(sellReserve,-obuy[buyOrder])))
+                sellReserve -= max(sellReserve,-obuy[buyOrder])
+                print(f'Market Taking Sell Order at {buyOrder} with volume {max(sellReserve,-obuy[buyOrder])}')
+            elif buyOrder == self.AMETHYST_PRICE and position > 0:
+                orders.append(Order(product, buyOrder, max(sellReserve,-obuy[buyOrder])))
+                sellReserve -= max(sellReserve,-obuy[buyOrder])
+                print(f'Market Taking Sell Order at {buyOrder} with volume {max(sellReserve,-obuy[buyOrder])}')
+               
+        # SELLING
+
+        print(f'Position: {position}')
+        ###################################################################
+
+        ## Market Making
+        best_bid = list(obuy.keys())[0]
+        best_ask = list(osell.keys())[0]
+        #sell order
+        orders.append(Order(product, max(best_ask-1, self.AMETHYST_PRICE+1), sellReserve))
+        print(f'Market Making Sell Order at {max(best_ask-1, self.AMETHYST_PRICE+1)} with volume {sellReserve}')
+        #buy order
+        orders.append(Order(product, min(best_bid+1, self.AMETHYST_PRICE-1), buyReserve))
+        print(f'Market Making Buy Order at {min(best_bid+1, self.AMETHYST_PRICE-1)} with volume {buyReserve}')
+        ###################################################################
+        
+        return orders
+
+    def run(self, state: TradingState) -> Dict[str, List[Order]]:
+        result = {}
+
+        timestamp = state.timestamp
+        LR_LOOKBACK = 16
+
+        if timestamp == 0:
+    
+            ## MARKET MAKING PARAMETERS
+            RISK_ADJUSTMENT = {"AMETHYSTS" : 0.1, "STARFRUIT" : 0.1}
+            ORDER_VOLUME = {"AMETHYSTS" : 4, "STARFRUIT" : 5}
+            HALF_SPREAD_SIZE = {"AMETHYSTS": 3, "STARFRUIT": 3}
+            ############################
+
+            ## ALGO PARAMETERS
+            prices = {
+                "asks" : {}, 
+                "bids" : {},
+                "avg_prices" : {},
+                "count" : {},
+                "acceptable_price" : {
+                    "AMETHYSTS" : 10000,
+                    "STARFRUIT" : 4895
+                } 
+            }
+            ############################
+
+            ## POSITION SIZING PARAMS
+            MM_POSITION_LIMIT = {"AMETHYSTS" : 20, "STARFRUIT" : 0}
+            MM_POSITION = {"AMETHYSTS" : 0, "STARFRUIT" : 0}
+            MM_LAST_ORDER_PRICE = {"AMETHYSTS" : {"BUY": 0, "SELL": 0}, "STARFRUIT" : {"BUY": 0, "SELL": 0}}
+            ############################
+            ALGO_POSITION_LIMIT = {"AMETHYSTS" : 0, "STARFRUIT" : 20}
+            ALGO_POSITION = {"AMETHYSTS" : 0, "STARFRUIT" : 0}
+            ALGO_LAST_ORDER_PRICE = {"AMETHYSTS" : {"BUY": 0, "SELL": 0}, "STARFRUIT" : {"BUY": 0, "SELL": 0}}
+            ############################
+            LAST_TIMESTAMP = -100000
+            PREV_PRICES = []
+            PREV_TIMESTAMPS = []
+
+        else:
+            traderData = jsonpickle.decode(state.traderData)
+            RISK_ADJUSTMENT, ORDER_VOLUME, HALF_SPREAD_SIZE, prices, MM_POSITION_LIMIT, MM_POSITION, MM_LAST_ORDER_PRICE, ALGO_POSITION_LIMIT, ALGO_POSITION, ALGO_LAST_ORDER_PRICE, LAST_TIMESTAMP, PREV_PRICES, PREV_TIMESTAMPS = traderData
+
+        for product in self.products:
+            orders: list[Order] = []
+            order_depth: OrderDepth = state.order_depths[product]
+
+            if product == 'AMETHYSTS':
+                try:
+                    curr_position = state.position[product]
+                except:
+                    curr_position = 0
+
+                if product == 'AMETHYSTS':
+                    orders = self.market_make_amethyst(product, order_depth, curr_position)
+                    result[product] = orders
+                    continue
+
+                result[product] = orders
+            else:
+
+
+                if len(order_depth.sell_orders) > 0 and len(order_depth.buy_orders) > 0:
+                    ##GET TRADES
+                    try:
+                        own_trades = state.own_trades[product]
+                    except:
+                        own_trades = []
+                    ##############################
+
+                    ## CALCULATING THE POSITION SIZE OF MARKET MAKING
+                    for trade in own_trades:
+                        if trade.timestamp == LAST_TIMESTAMP:
+                            if trade.buyer == "SUBMISSION" and trade.price == MM_LAST_ORDER_PRICE[product]["BUY"]:
+                                MM_POSITION[product] += trade.quantity
+                            elif trade.seller == "SUBMISSION" and trade.price == MM_LAST_ORDER_PRICE[product]["SELL"]:
+                                MM_POSITION[product] -= trade.quantity
+                    ##############################
+
+                    ## CALCULATING THE POSITION SIZE OF ALGO
+                    for trade in own_trades:
+                        if trade.timestamp == LAST_TIMESTAMP:
+                            if trade.buyer == "SUBMISSION" and trade.price == ALGO_LAST_ORDER_PRICE[product]["BUY"]:
+                                ALGO_POSITION[product] += trade.quantity
+                            elif trade.seller == "SUBMISSION" and trade.price == ALGO_LAST_ORDER_PRICE[product]["SELL"]:
+                                ALGO_POSITION[product] -= trade.quantity
+                    ##############################
+
+                    ## CALCULATING STATS
+                    best_bid = max(order_depth.buy_orders.keys())
+                    best_ask = min(order_depth.sell_orders.keys())
+                    best_bid_volume = order_depth.buy_orders[best_bid]
+                    best_ask_volume = order_depth.sell_orders[best_ask]
+                    value = (best_ask + best_bid)/2
+                    ##############################
+
+                    ## MARKET MAKING STRATEGY
+                    skew = -MM_POSITION[product] * RISK_ADJUSTMENT[product]
+                    buy_quote = floor(value - HALF_SPREAD_SIZE[product] + skew)
+                    sell_quote = floor(value + HALF_SPREAD_SIZE[product] + skew)
+                    ##############################
+
+                    ## ALGO STRATEGY
+
+                    if product not in prices["asks"]:
+                        prices["asks"][product] = []
+                    if product not in prices["bids"]:
+                        prices["bids"][product] = []
+                    
+                    prices["asks"][product].append(best_ask)
+                    prices["bids"][product].append(best_bid)
+
+                    curr_price = value
+
+                    # first n iterations
+                    if len(PREV_PRICES) < LR_LOOKBACK + 1:
+                        prices["avg_prices"][product] = [curr_price]
+                        prices["acceptable_price"][product] = curr_price
+
+                        # don't place orders in the first iteration
+                        result[product] = orders
+                        PREV_PRICES.append(curr_price)
+                        PREV_TIMESTAMPS.append(state.timestamp)
+                        continue
+
+                    PREV_PRICES.pop(0)
+                    PREV_TIMESTAMPS.pop(0)
+
+                    PREV_PRICES.append(curr_price)
+                    PREV_TIMESTAMPS.append(state.timestamp)
+
+                    prev_percentages = [(PREV_PRICES[i + 1] - PREV_PRICES[i])/PREV_PRICES[i] + 1 for i in range(len(PREV_PRICES) - 1)]
+
+
+                    m, b = linear_regression(PREV_TIMESTAMPS[1:], prev_percentages)
+
+                    next_time = state.timestamp + (PREV_TIMESTAMPS[-1] - PREV_TIMESTAMPS[-2])
+
+                    change_in_price = m * next_time + b
+
+                    acceptable_price = change_in_price * curr_price
+
+                    prices["acceptable_price"][product] = acceptable_price
+                    prices["avg_prices"][product].append(acceptable_price)
+
+                    ##############################
+
+                    ## MARKET MAKING ORDERS
+                    orders.append(Order(product, buy_quote, max(0,min(ORDER_VOLUME[product], MM_POSITION_LIMIT[product] - MM_POSITION[product]))))
+                    orders.append(Order(product, sell_quote, -max(0,min(ORDER_VOLUME[product], MM_POSITION_LIMIT[product] + MM_POSITION[product]))))
+                    MM_LAST_ORDER_PRICE[product] = {"BUY": buy_quote, "SELL": sell_quote}
+                    ##############################
+
+                    ## ALGO ORDERS
+                    if best_ask < (acceptable_price - 3) and best_ask != buy_quote: #second part of and is so that orders don't overlap, which lets me individually keep track of positions
+                        # print("BUY", str(-best_ask_volume) + "x", best_ask)
+                        orders.append(Order(product, best_ask, max(0,min(-best_ask_volume, ALGO_POSITION_LIMIT[product] - ALGO_POSITION[product]))))
+                        ALGO_LAST_ORDER_PRICE[product]["BUY"] = best_ask
+                    else:
+                        ALGO_LAST_ORDER_PRICE[product]["BUY"] = 0
+
+                    if best_bid > (acceptable_price + 3) and best_bid != sell_quote: #second part of and is so that orders don't overlap, which lets me individually keep track of positions
+                        # print("SELL", str(best_bid_volume) + "x", best_bid)
+                        orders.append(Order(product, best_bid, -max(0,min(best_bid_volume, ALGO_POSITION_LIMIT[product] + ALGO_POSITION[product]))))
+                        ALGO_LAST_ORDER_PRICE[product]["SELL"] = best_bid
+                    else:
+                        ALGO_LAST_ORDER_PRICE[product]["SELL"] = 0
+                    ##############################
+                    
+                    ## PRINT STATS
+                    print(f'{product}:')
+                    
+                    try:
+                        position = state.position[product]
+                    except:
+                        position = 0
+                    
+                    print(f'Actual position: {position}')
+                    print('Estimated MM position: ', MM_POSITION[product])
+                    print('Estimated ALGO position: ', ALGO_POSITION[product])
+                    ##############################
+                    
+                    result[product] = orders
+
+        LAST_TIMESTAMP = state.timestamp
+        
+        print('\n----------------------------------------------------------------------------------------------------\n')
+        params = [RISK_ADJUSTMENT, ORDER_VOLUME, HALF_SPREAD_SIZE, prices, MM_POSITION_LIMIT, MM_POSITION, 
+                  MM_LAST_ORDER_PRICE, ALGO_POSITION_LIMIT, ALGO_POSITION, ALGO_LAST_ORDER_PRICE, LAST_TIMESTAMP, PREV_PRICES, PREV_TIMESTAMPS]
+        traderData = jsonpickle.encode(params) # String value holding Trader state data required. It will be delivered as TradingState.traderData on next execution.
+        conversions = 0
+
+        return result, conversions, traderData
